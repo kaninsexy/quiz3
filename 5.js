38072015f0ed135a4be89936748c57edffe0c4e3
KANIN SRIJUNDORN <kaninsrijundorn@MacBook-Pro.local>
2020-07-03 13:21:36 +0700

    asd

--- 5.js
+++ 5.js
@@ -5,5 +5,48 @@ function changeStringtoThaiDate(word) {
   let date = new Date();
   let testDate = `${month}-${day}-${year}`;
   date = date.setFullYear(year + 543, month - 1, day);
-  Date.parse(testDate) === NaN ? console.log('invalid') : console.log(date);
+  switch (month) {
+    case 1:
+      'มกราคม';
+      break;
+    case 2:
+      'กุมภาพันธ์';
+      break;
+    case 3:
+      'มีนาคม';
+      break;
+    case 4:
+      'เมษายน';
+      break;
+    case 5:
+      'พฤษภาคม';
+      break;
+    case 6:
+      'มิถุนายน';
+      break;
+    case 7:
+      'กรกฎาคม';
+      break;
+    case 8:
+      'สิงหาคม';
+      break;
+    case 9:
+      'กันยายน';
+      break;
+    case 10:
+      'ตุลาคม';
+      break;
+    case 11:
+      'พฤศจิกายน';
+      break;
+    case 12:
+      'ธันวาคม';
+      break;
+
+    default:
+      break;
+  }
+  Date.parse(testDate) === NaN
+    ? console.log('invalid')
+    : console.log(`${day} ${month} ${year}`);
 }
