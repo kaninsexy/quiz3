1e845d9478c9b483c5b9445b83025f2df3fd9531
KANIN SRIJUNDORN <kaninsrijundorn@MacBook-Pro.local>
2020-07-03 09:36:18 +0700

    fix date

--- 5.js
+++ 5.js
@@ -3,9 +3,7 @@ function changeStringtoThaiDate(word) {
   let month = Number(word.split('-')[1]);
   let year = Number(word.split('-')[2]);
   let date = new Date();
-  if (month > 13 || (day > 29 && month == 2)) {
-    console.log('invalid');
-  } else {
-    date.setFullYear(year + 543, month - 1, day);
-  }
+  let testDate = `${month}-${day}-${year}`;
+  date = date.setFullYear(year + 543, month - 1, day);
+  Date.parse(testDate) === NaN ? console.log('invalid') : console.log(date);
 }
